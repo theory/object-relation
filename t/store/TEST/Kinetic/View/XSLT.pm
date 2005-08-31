97c63aeacd6e60491374439c71f982016c1fd4ac
Curtis Poe <curtis_ovid_poe@yahoo.com>
2005-08-31 18:22:01 +0000

    Merely some reformatting of TEST::Kinetic::View::XSLT

--- t/store/TEST/Kinetic/View/XSLT.pm
+++ t/store/TEST/Kinetic/View/XSLT.pm
@@ -11,7 +11,7 @@ use Test::Exception;
 use Test::XML;
 
 use Kinetic::XML;
-use Kinetic::Util::Constants  qw/UUID_RE CURRENT_PAGE/;
+use Kinetic::Util::Constants qw/UUID_RE CURRENT_PAGE/;
 use Kinetic::Util::Exceptions qw/sig_handlers/;
 BEGIN { sig_handlers(0) }
 
@@ -20,25 +20,26 @@ use aliased 'Test::MockModule';
 use aliased 'Kinetic::Store' => 'Store', ':all';
 
 use aliased 'TestApp::Simple::One';
-use aliased 'TestApp::Simple::Two'; # contains a TestApp::Simple::One object
+use aliased 'TestApp::Simple::Two';    # contains a TestApp::Simple::One object
 
 __PACKAGE__->SKIP_CLASS(
     __PACKAGE__->any_supported(qw/pg sqlite/)
-      ? 0
-      : "Not testing Data Stores"
-) if caller; # so I can run the tests directly from vim
+    ? 0
+    : "Not testing Data Stores"
+  )
+  if caller;    # so I can run the tests directly from vim
 __PACKAGE__->runtests unless caller;
 
 sub setup : Test(setup) {
-    my $test = shift;
+    my $test  = shift;
     my $store = Store->new;
     $test->{dbh} = $store->_dbh;
     $test->{dbh}->begin_work;
-    $test->{dbi_mock} = MockModule->new('DBI::db', no_auto => 1);
-    $test->{dbi_mock}->mock(begin_work => 1);
-    $test->{dbi_mock}->mock(commit => 1);
+    $test->{dbi_mock} = MockModule->new( 'DBI::db', no_auto => 1 );
+    $test->{dbi_mock}->mock( begin_work => 1 );
+    $test->{dbi_mock}->mock( commit     => 1 );
     $test->{db_mock} = MockModule->new('Kinetic::Store::DB');
-    $test->{db_mock}->mock(_dbh => $test->{dbh});
+    $test->{db_mock}->mock( _dbh => $test->{dbh} );
     my $foo = One->new;
     $foo->name('foo');
     $store->save($foo);
@@ -48,87 +49,86 @@ sub setup : Test(setup) {
     my $baz = One->new;
     $baz->name('snorfleglitz');
     $store->save($baz);
-    $test->{test_objects} = [$foo, $bar, $baz];
+    $test->{test_objects} = [ $foo, $bar, $baz ];
     $test->{param} = sub {
-        my $self = shift;
-        my @query_string = @{$test->_query_string || []};
-        my %param = @query_string;
+        my $self         = shift;
+        my @query_string = @{ $test->_query_string || [] };
+        my %param        = @query_string;
         if (@_) {
-            return $param{+shift};
+            return $param{ +shift };
         }
         else {
             my $i = 0;
-            return grep { ! ($i++ % 2) } @query_string;
+            return grep { !( $i++ % 2 ) } @query_string;
         }
     };
 }
 
 sub teardown : Test(teardown) {
     my $test = shift;
-    delete($test->{dbi_mock})->unmock_all;
+    delete( $test->{dbi_mock} )->unmock_all;
     $test->{dbh}->rollback unless $test->{dbh}->{AutoCommit};
-    delete($test->{db_mock})->unmock_all;
+    delete( $test->{db_mock} )->unmock_all;
 }
 
 sub constructor : Test(6) {
     my $test = shift;
     can_ok XSLT, 'new';
-    throws_ok {XSLT->new}
-        'Kinetic::Util::Exception::Fatal::RequiredArguments',
-        'Calling new() without a type argument should throw an exception';
+    throws_ok { XSLT->new }
+      'Kinetic::Util::Exception::Fatal::RequiredArguments',
+      'Calling new() without a type argument should throw an exception';
 
-    throws_ok {XSLT->new(type => 'no_such_type')}
-        'Kinetic::Util::Exception::Fatal::Unsupported',
-        'Calling new() with an unknown type should throw an exception';
+    throws_ok { XSLT->new( type => 'no_such_type' ) }
+      'Kinetic::Util::Exception::Fatal::Unsupported',
+      'Calling new() with an unknown type should throw an exception';
 
-    ok my $xslt = XSLT->new(type => 'instance'),
-        '... and calling it with a known type should succeed';
+    ok my $xslt = XSLT->new( type => 'instance' ),
+      '... and calling it with a known type should succeed';
     isa_ok $xslt, XSLT, '... and the object it returns';
     is $xslt->type, 'instance', '... and it should have the correct XSLT type';
 }
 
 sub type : Test(5) {
     my $test = shift;
-    my $xslt = XSLT->new(type => 'instance');
+    my $xslt = XSLT->new( type => 'instance' );
     can_ok $xslt, 'type';
 
-    throws_ok {$xslt->type('no_such_type')}
-        'Kinetic::Util::Exception::Fatal::Unsupported',
-        '... and calling it with an unknown type should throw an exception';
-    is $xslt->type, 'instance', 
-        '... and it should return the type set in the constructor';
+    throws_ok { $xslt->type('no_such_type') }
+      'Kinetic::Util::Exception::Fatal::Unsupported',
+      '... and calling it with an unknown type should throw an exception';
+    is $xslt->type, 'instance',
+      '... and it should return the type set in the constructor';
     ok $xslt->type('REST'),
-        'Calling type() with a valid XSLT type should succeed';
-    is $xslt->type, 'REST','... setting the type to the requested value';
-} 
+      'Calling type() with a valid XSLT type should succeed';
+    is $xslt->type, 'REST', '... setting the type to the requested value';
+}
 
 sub stylesheet : Test(3) {
     my $test = shift;
-    my $xslt = XSLT->new(type => 'REST');
+    my $xslt = XSLT->new( type => 'REST' );
     can_ok $xslt, 'stylesheet';
 
     my $xml = $xslt->stylesheet;
-    is_well_formed_xml $xml, 
-        '... and it should return valid XML for "REST"';
-    
+    is_well_formed_xml $xml, '... and it should return valid XML for "REST"';
+
     $xslt->type('instance');
     $xml = $xslt->stylesheet;
-    is_well_formed_xml $xml, 
-        '... and it should return valid XML for "instance"';
+    is_well_formed_xml $xml,
+      '... and it should return valid XML for "instance"';
 }
 
 sub transform : Test(9) {
     my $test = shift;
-    my $xslt = XSLT->new(type => 'REST');
+    my $xslt = XSLT->new( type => 'REST' );
     can_ok $xslt, 'transform';
 
-    throws_ok {$xslt->transform}
-        'Kinetic::Util::Exception::Fatal::RequiredArguments',
-        'Calling transform() without xml should throw an exception';
+    throws_ok { $xslt->transform }
+      'Kinetic::Util::Exception::Fatal::RequiredArguments',
+      'Calling transform() without xml should throw an exception';
 
-    throws_ok {$xslt->transform("This ain't xml")}
-        'Kinetic::Util::Exception::ExternalLib',
-        '... as should calling it with an argument that is not valid XML';
+    throws_ok { $xslt->transform("This ain't xml") }
+      'Kinetic::Util::Exception::ExternalLib',
+      '... as should calling it with an argument that is not valid XML';
 
     my $xml = <<'    END_XML';
 <?xml version="1.0"?>
@@ -141,8 +141,8 @@ sub transform : Test(9) {
       <kinetic:resource id="XXX" xlink:href="http://somehost.com/rest/one/XXX"/>
     </kinetic:resources>
     END_XML
-    ok my $xhtml = $xslt->transform($xml), 
-        'Calling transform() with valid XML should succeed';
+    ok my $xhtml = $xslt->transform($xml),
+      'Calling transform() with valid XML should succeed';
 
     is_xml $xhtml, <<'    END_XHTML', '... and return the correct xhtml';
 <?xml version="1.0"?>
@@ -163,7 +163,7 @@ sub transform : Test(9) {
   </body>
 </html>
     END_XHTML
-    
+
     $xml = <<'    END_XML';
 <?xml version="1.0"?>
 <?xml-stylesheet type="text/xsl" href="http://somehost.com/rest/?stylesheet=browse"?>
@@ -175,9 +175,9 @@ sub transform : Test(9) {
       <kinetic:resource id="two" xlink:href="http://somehost.com/rest/two"/>
     </kinetic:resources>
     END_XML
-    
-    ok $xhtml = $xslt->transform($xml), 
-        'Calling transform() with valid XML should succeed';
+
+    ok $xhtml = $xslt->transform($xml),
+      'Calling transform() with valid XML should succeed';
 
     is_xml $xhtml, <<'    END_XHTML', '... and return the correct xhtml';
 <?xml version="1.0"?>
@@ -198,12 +198,12 @@ sub transform : Test(9) {
   </body>
 </html>
     END_XHTML
-    my ($foo, $bar, $baz) = @{$test->{test_objects}};
-    $xml = Kinetic::XML->new({object => $foo})->dump_xml;
+    my ( $foo, $bar, $baz ) = @{ $test->{test_objects} };
+    $xml = Kinetic::XML->new( { object => $foo } )->dump_xml;
 
     $xslt->type('instance');
-    ok $xhtml = $xslt->transform($xml), 
-        'Calling transform() with valid XML should succeed';
+    ok $xhtml = $xslt->transform($xml),
+      'Calling transform() with valid XML should succeed';
 
     is_xml $xhtml, <<"    END_XHTML", '... and return the correct xhtml';
 <?xml version="1.0"?>
@@ -246,7 +246,7 @@ sub transform : Test(9) {
 
 sub transform_pages : Test(5) {
     my $test = shift;
-    my $xslt = XSLT->new(type => 'REST');
+    my $xslt = XSLT->new( type => 'REST' );
 
     my $xml = <<'    END_XML';
 <?xml version="1.0"?>
@@ -262,8 +262,8 @@ sub transform_pages : Test(5) {
       </kinetic:pages>
     </kinetic:resources>
     END_XML
-    ok my $xhtml = $xslt->transform($xml), 
-        'Calling transform() with XML that has pages should succeed';
+    ok my $xhtml = $xslt->transform($xml),
+      'Calling transform() with XML that has pages should succeed';
 
     is_xml $xhtml, <<'    END_XHTML', '... and return xml with pages';
 <?xml version="1.0"?>
@@ -296,7 +296,7 @@ sub transform_pages : Test(5) {
       </body>
     </html>
     END_XHTML
-    
+
     $xml = <<"    END_XML";
 <?xml version="1.0"?>
 <?xml-stylesheet type="text/xsl" href="http://localhost:9000/rest/?stylesheet=REST"?>
@@ -311,8 +311,8 @@ sub transform_pages : Test(5) {
       </kinetic:pages>
     </kinetic:resources>
     END_XML
-    ok $xhtml = $xslt->transform($xml), 
-        'Calling transform() with XML that has pages should succeed';
+    ok $xhtml = $xslt->transform($xml),
+      'Calling transform() with XML that has pages should succeed';
 
     is_xml $xhtml, <<"    END_XHTML", '... and return xml with pages';
 <?xml version="1.0"?>
@@ -345,10 +345,9 @@ sub transform_pages : Test(5) {
       </body>
     </html>
     END_XHTML
-    
-    ok $xhtml = $xslt->transform($xml), 
-        '... but it should not create page links to the current page';
 
+    ok $xhtml = $xslt->transform($xml),
+      '... but it should not create page links to the current page';
 }
 
 1;
