d2263eb4db783da59fd54baf9a79f53cc375d1e1
Curtis Poe <curtis_ovid_poe@yahoo.com>
2005-11-02 23:25:50 +0000

    Dumper module versions

---
+++ etc/versions.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib 'lib', 't/store', 't/lib', 't/sample/lib';
+use Data::Dumper; 
+use TEST::Kinetic::View::XSLT;
+use TEST::Kinetic::Interface::REST;
+use TEST::Kinetic::Store;
+use Kinetic::Util::Exceptions qw/sig_handlers/;
+BEGIN { sig_handlers(0) }
+
+foreach my $module (sort keys %INC) {
+    $module =~ s/\.pm$//;
+    $module =~ s/\//::/g;
+    next if $module =~ /^::/;
+    eval "use $module";
+    if ($@) {
+        print "Cannot use ($module): $@";
+    }
+    else {
+        printf "$module->VERSION %s\n", $module->VERSION || 'unknown';
+    }
+}
