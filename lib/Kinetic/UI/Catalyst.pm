6b4245848d49f14301541f55930a46417a129d80
Curtis Poe <curtis_ovid_poe@yahoo.com>
2005-12-14 07:30:09 +0000

    Oops.  Forgot to commit this.

---
+++ lib/Kinetic/UI/Catalyst.pm
@@ -0,0 +1,103 @@
+package Kinetic::UI::Catalyst;
+
+use strict;
+use warnings;
+
+#
+# Set flags and add plugins for the application
+#
+#         -Debug: activates the debug mode for very useful log messages
+# Static::Simple: will serve static files from the applications root directory
+#
+use Catalyst qw/
+  -Debug
+  Authentication
+  Authentication::Store::Minimal
+  Authentication::Credential::Password
+  Session
+  Session::Store::File
+  Session::State::Cookie
+  Static::Simple
+  /;
+
+our $VERSION = '0.01';
+
+__PACKAGE__->config(
+    name             => 'Kinetic Catalyst Interface',
+    'V::TToolkit' =>
+      { INCLUDE_PATH => __PACKAGE__->path_to('www/templates/tt') },
+    authentication => {
+        users => {
+            ovid   => { password => 'divo' },
+            theory => { password => 'theory' }
+        }
+    }
+);
+
+__PACKAGE__->setup;
+
+=head1 NAME
+
+Kinetic::UI::Catalyst - Catalyst UI for Kinetic Platform
+
+=head1 SYNOPSIS
+
+    script/server.pl
+
+=head1 DESCRIPTION
+
+Catalyst based application.
+
+=head1 METHODS
+
+=head2 default
+
+=cut
+
+sub begin : Private {
+    my ( $self, $c ) = @_;
+
+    # force login for all pages
+    unless ( $c->user ) {
+        $c->req->action(undef);
+        $c->forward( 'Kinetic::UI::Catalyst::C::Login', 'login' );
+    }
+}
+
+#
+# Output a friendly welcome message
+#
+
+sub default : Private {
+    my ( $self, $c ) = @_;
+
+    # Hello World
+    $c->response->body( $c->welcome_message );
+}
+
+#
+# Uncomment and modify this end action after adding a View component
+#
+#=head2 end
+#
+#=cut
+#
+#sub end : Private {
+#    my ( $self, $c ) = @_;
+#
+#    # Forward to View unless response body is already defined
+#    $c->forward( $c->view('') ) unless $c->response->body;
+#}
+
+=head1 AUTHOR
+
+Curtis Poe
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
