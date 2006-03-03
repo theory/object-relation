f5c3bbce87b0a5b9e3a74316ce1e3788b3c5baea
Curtis Poe <curtis_ovid_poe@yahoo.com>
2006-03-03 00:19:40 +0000

    Corrected bad test count.
    The failure in t/build.t was a holdover from the failure in t/app_build.t 
    failing and not cleaning up t/sample/t/data/kinetic.db.  Once that file was
    removed, t/build.t passed all tests.  Now looking at t/app_build.t

--- t/build/TEST/Kinetic/Build/Setup/Store/DB/SQLite.pm
+++ t/build/TEST/Kinetic/Build/Setup/Store/DB/SQLite.pm
@@ -33,7 +33,7 @@ sub test_class_methods : Test(8) {
     ok $class->rules, "We should get some rules";
 }
 
-sub test_rules : Test(44) {
+sub test_rules : Test(45) {
     my $self  = shift;
     my $class = $self->test_class;
 
