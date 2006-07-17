f64a1b100cffcf6f1ca5c80f5f38bf14100763da
David Wheeler <david@justatheory.com>
2006-07-17 20:43:37 +0000

    Switch to Perl license.

--- lib/Kinetic/Store/Parser.pm
+++ lib/Kinetic/Store/Parser.pm
@@ -2,36 +2,6 @@ package Kinetic::Store::Parser;
 
 # $Id$
 
-# CONTRIBUTION SUBMISSION POLICY:
-#
-# (The following paragraph is not intended to limit the rights granted to you
-# to modify and distribute this software under the terms of the GNU General
-# Public License Version 2, and is only of importance to you if you choose to
-# contribute your changes and enhancements to the community by submitting them
-# to Kineticode, Inc.)
-#
-# By intentionally submitting any modifications, corrections or
-# derivatives to this work, or any other work intended for use with the
-# Kinetic framework, to Kineticode, Inc., you confirm that you are the
-# copyright holder for those contributions and you grant Kineticode, Inc.
-# a nonexclusive, worldwide, irrevocable, royalty-free, perpetual license to
-# use, copy, create derivative works based on those contributions, and
-# sublicense and distribute those contributions and any derivatives thereof.
-
-#
-# XXX Developers:  you may wish to read the Kinetic::Store::Parser::Overview
-# document to better understand what is happening here.
-#
-# Each rule in the grammar from the overview document has been copied to this
-# module as comments in front of each rule implementation.  Please keep these
-# in synch.
-#
-# Also, there are a fair number of comments in this code to help you understand
-# the tricky bits.  If you really get stuck, buy "Higher Order Perl" and read
-# Chapter 8.  Many of the subparsers here are extensions to that work, but they
-# should be fairly easy to understand.
-#
-
 use strict;
 
 use version;
@@ -437,15 +407,7 @@ __END__
 
 Copyright (c) 2004-2006 Kineticode, Inc. <info@kineticode.com>
 
-This work is made available under the terms of Version 2 of the GNU General
-Public License. You should have received a copy of the GNU General Public
-License along with this program; if not, download it from
-L<http://www.gnu.org/licenses/gpl.txt> or write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-This work is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-A PARTICULAR PURPOSE. See the GNU General Public License Version 2 for more
-details.
+This module is free software; you can redistribute it and/or modify it under the
+same terms as Perl itself.
 
 =cut
